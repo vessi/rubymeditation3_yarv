a1063ced1d3f79f8dcab3d0626fa93855685ee5b
Mikhail Bortnyk <vessimir@gmail.com>
2013-11-02 00:34:41 +0200

    Added sample with syntax error to show that lexical parses doesnot check this

--- meditation1.rb
+++ meditation1.rb
@@ -1,10 +1,20 @@
 require 'pp'
 require 'ripper'
+# one line
 code = "10.times { |n| puts n }"
 puts code
 pp Ripper.lex(code)
+# multi line
 code = <<STR
 10.times do |n|
+  puts n
+end
+STR
+puts code
+pp Ripper.lex(code)
+# syntax error
+code = <<STR
+10.times do |n
   puts n
 end
 STR
